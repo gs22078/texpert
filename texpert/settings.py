0e0373a492e29a09df1f57cd138a703bb26e12b6
이준석 <gs22078@gs.hs.kr>
2023-09-02 17:29:47 +0900

    Add requirements.txt

--- texpert/settings.py
+++ texpert/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-tg94!j(3ii61yvzg%ra4%v2nwb3_k-88g4ly3#&nwvw&5*p3!q'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['texpert.azurewebsites.net']
 
 
 # Application definition
