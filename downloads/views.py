080a65dee0e4e97dab9fe931f2048cb4472366de
이준석 <gs22078@gs.hs.kr>
2023-09-04 23:08:10 +0900

    *

--- downloads/views.py
+++ downloads/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 import requests
+import os
 
 repos = [
     {'owner': 'gshsTeXperT', 'repo': 'TeXperT-templates'},
@@ -8,8 +9,20 @@ repos = [
 ]
 
 
+def github_api_requests(url):
+    #github_token = os.environ['GITHUB_TOKEN']
+    #headers = {'Authorization': f'token {github_token}'}
+    return requests.get(url)#, headers=headers)
+
+
 def index(request):
-    context = {'repos': repos}
+    tmp = []
+    for repo in repos:
+        req = github_api_requests(f'https://api.github.com/repos/{repo["owner"]}/{repo["repo"]}').json()
+        avatar = req['owner']['avatar_url']
+        description = req['description']
+        tmp.append({'owner': repo['owner'], 'repo': repo['repo'], 'avatar': avatar, 'description': description})
+    context = {'repos': tmp, 'print': os.environ}
     return render(request, 'downloads/index.html', context)
 
 
